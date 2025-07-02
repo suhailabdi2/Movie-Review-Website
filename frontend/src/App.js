3c535ebb287c807c6984fddf225af16ba685a531
Suhail Abdi <suhailabdi857@gmail.com>
2025-07-02 11:45:17 +0300

    integrated react router so that I can be able to add multiple routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,55 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect,useState } from 'react';
+import { useEffect,useState,useNavigate } from 'react';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
-
-  const [movies,setMovies] = useState('');
-  const [tvs,setTV]= useState('');
-  const baseURL = "https://image.tmdb.org/t/p/w500";
-  const url = 'https://api.themoviedb.org/3/discover/movie';
-  const options = {
-    method: 'GET',
-    key:process.env.REACT_APP_API_KEY,
-    headers:{
-      accept:'application/json',
-      Authorization:`Bearer ${process.env.REACT_APP_API_KEY}`
-    }
-  };
-  useEffect(()=> {
-    fetch(url, options)
-    .then(res => res.json())
-    .then(json => {
-      setMovies(json.results);
-      console.log(json.results);
-    })
-    .catch(err => console.error(err));
-  },[])
-  useEffect(()=>{
-    fetch('https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc',options)
-    .then(res=>res.json())
-    .then(json =>{
-      setTV(json.results);
-      console.log(json.results)
-    })
-    .catch(err => console.error(err));
-  },[])
   return (
-   <div>
-    <h1> Best Movies</h1>
-      <ul>
-        {movies && movies.map(
-          (mov,index )=> <li key={index}>{mov.original_title} :language in {mov.original_language} imdb rating <meter min="0" max="10" value={mov.vote_average}></meter><br/> <img src={`${baseURL+mov.poster_path}`}/></li>
-        )}
-      </ul>
-      <h1>Best TV Series</h1>
-      <ul>
-        {tvs && tvs.map((tv,index)=>
-          <li key={index}>{tv.name}<meter min="0" max="10" value={tv.vote_average} /></li>
-        ) }
-      </ul>
-
-   </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import * as dotenv from 'dotenv';
-//require(dotenv).config()
+// require(dotenv).config()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
